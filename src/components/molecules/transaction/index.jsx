c5c3750b753aad011345823c8764033fca60884b
Wesley Wienand <wesley@codehesion.co.za>
2022-03-09 13:37:44 +0000

    Add Payment Type to transactions

--- src/components/molecules/transaction/index.jsx
+++ src/components/molecules/transaction/index.jsx
@@ -1,38 +1,28 @@
 import React from 'react';
 import { StyleSheet } from 'react-native';
-import { ListItem } from 'react-native-elements';
+import { ListItem, Divider } from 'react-native-elements';
 import _ from 'lodash';
-import Moment from 'moment';
 import PropTypes from 'prop-types';
 
+import { paymentTransactionModel } from '../../../models/app/payment/payment.model';
+
 const Transaction = ({ transaction }) => {
-  const getPayableNumber = (item) => {
-    const bank = _.get(item, 'payable.bank_account.account_number', null);
-    const creditCard = _.get(item, 'payable.credit_card.obfuscated_card_number', null);
-
-    if (!_.isNull(bank)) {
-      return bank;
-    }
-    return creditCard;
-  };
-
-  const paymentProvider = _.get(transaction, 'payment_provider');
-  const payableType = _.startCase(_.get(transaction, 'payable_type'));
-  const updatedAt = Moment(_.get(transaction, 'updated_at')).format('YYYY/MM/DD, HH:MM');
-  const payable = getPayableNumber(transaction);
-  const amount = (_.get(transaction, 'total.cents') / 100).toFixed(2);
-  const status = _.capitalize(_.get(transaction, 'status'));
+  const transactionData = paymentTransactionModel(transaction);
 
   return (
     <ListItem key={_.get(transaction, 'id')} bottomDivider>
       <ListItem.Content>
-        <ListItem.Title>{`${paymentProvider} ${payableType}`}</ListItem.Title>
-        <ListItem.Subtitle>{updatedAt}</ListItem.Subtitle>
-        {payable && <ListItem.Subtitle>{payable}</ListItem.Subtitle>}
+        <ListItem.Title>{`${transactionData.paymentProvider} ${transactionData.payableType}`}</ListItem.Title>
+        <ListItem.Subtitle>{transactionData.paymentType}</ListItem.Subtitle>
+        <ListItem.Subtitle>{transactionData.updatedAt}</ListItem.Subtitle>
+        {transactionData.payable && (
+          <ListItem.Subtitle>{transactionData.payable}</ListItem.Subtitle>
+        )}
       </ListItem.Content>
       <ListItem.Content style={styles.alignRight}>
-        <ListItem.Title h4>{amount}</ListItem.Title>
-        <ListItem.Subtitle>{status}</ListItem.Subtitle>
+        <Divider />
+        <ListItem.Title h4>{transactionData.amount}</ListItem.Title>
+        <ListItem.Subtitle>{transactionData.status}</ListItem.Subtitle>
       </ListItem.Content>
     </ListItem>
   );
